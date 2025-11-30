7493244750b9983ff178641e0ae66dc831a1a9e7
Anonymous20058 <tanvirhossien13@gmail.com>
2025-11-30 22:53:27 +0600

    Start Day-7

---
+++ Day-7/index.js
@@ -0,0 +1,10 @@
+// 🎯 Day 7 – Prime Factorization & Composite Numbers
+// 🧩 Core Concept Focus
+// Understanding prime factorization
+// Identifying distinct prime factors
+// Classification of numbers using their factor patterns
+// Strengthening number theory reasoning using loops, modulo, and mathematical logic
+// 🏫 Class Questions
+// 1. Find the Prime Factorization (Return as an Array)
+// Input: N = 84
+// Output: [2, 2, 3, 7]
