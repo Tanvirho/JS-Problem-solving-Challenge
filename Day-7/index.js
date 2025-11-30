68783e4324c9c8310895e64c97e17344e1405a27
Anonymous20058 <tanvirhossien13@gmail.com>
2025-11-30 23:32:15 +0600

    add solution Q.1

--- Day-7/index.js
+++ Day-7/index.js
@@ -5,6 +5,48 @@
 // Classification of numbers using their factor patterns
 // Strengthening number theory reasoning using loops, modulo, and mathematical logic
 // 🏫 Class Questions
+
 // 1. Find the Prime Factorization (Return as an Array)
 // Input: N = 84
 // Output: [2, 2, 3, 7]
+
+// function primeFactorization(num) {
+//   const prime = [];
+//   let factor = 2;
+//   while (factor <= Math.sqrt(num)) {
+//     while (num % factor === 0) {
+//       prime.push(factor);
+//       num = num / factor;
+//     }
+//     factor++;
+//   }
+//   if (num > 2) prime.push(num);
+//   return prime;
+// }
+
+// or
+
+function primeFactorization(num) {
+  const prime = [];
+
+  while (num % 2 === 0) {
+    prime.push(2);
+    num = num / 2;
+  }
+  let factor = 3;
+  while (factor <= Math.sqrt(num)) {
+    while (num % factor === 0) {
+      prime.push(factor);
+      num = num / factor;
+    }
+  }
+  factor += 2;
+  if (num > 2) prime.push(num);
+  return prime;
+}
+
+console.log(primeFactorization(84));
+
+// 2. Factorization in Exponent Form
+// Input: N = 360
+// Output: "2^3 × 3^2 × 5^1"
